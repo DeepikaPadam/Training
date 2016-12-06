ac225ed370eef081a4a5d95c2cfe4fd5964e89fd
Mani Deepika Padam <c_mpadam@innominds.com>
2016-12-06 09:46:34 +0530

    LESS concepts

--- Less/lessController.js
+++ Less/lessController.js
@@ -1,23 +1,24 @@
 var app = angular.module('myApp',[]);
 
-app.service('jsonService', function($http) {
-this.getData = function(callbackFunc) {
-
-    $http.get("lessJson.json").success(function(data){
-    	console.log(data)
-        callbackFunc(data);
-       
-    }).error(function(){
-        alert("error");
-    });
- }
+app.service('jsonService', function ($http, $q) {
+    this.getData = function() {
+        return $http.get("lessJson.json")
+            .then(function(response) {
+                console.log(response.data)
+                return response.data; 
+            }, function(error) {
+                return $q.reject(error);
+            });
+    }
 });
+app.controller('serviceController', function($scope, jsonService, $q) {
+    
+    jsonService.getData()       
+        .then(function(response) {
+            console.log(response)           
+            $scope.data = response;
+        }, function(error) {          
+            alert("Data not found");
+        });
 
-app.controller('serviceController', function($scope, jsonService) {
-    $scope.data = null;
-    jsonService.getData(function(dataResponse) {
-        $scope.data = dataResponse;
-        console.log($scope.data)
-    });
-
-});
+}); 
