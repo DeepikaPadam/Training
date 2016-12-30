b39c32fde18ad215050d7aef45e2416e1b2ca668
DeepikaPadam <manideepika.padam@gmail.com>
2016-12-30 11:57:20 +0530

    Guess the number game

---
+++ Game/app.js
@@ -0,0 +1,43 @@
+var app = angular.module("myApp", []);
+
+
+app.controller("gameController", function($scope) {
+
+    startGame();
+
+    function startGame() {
+
+        $scope.input = '';
+        $scope.display = true;
+
+        var randomNumber = Math.round((Math.random() * 10) * 10);
+        console.log(randomNumber);
+        $scope.text = "Choose a number between 1 to 100";
+
+        $scope.onClick = function() {
+
+            if ($scope.input == randomNumber) {
+                $scope.text = "Great Guessing..! You won";
+                $scope.display = false;
+
+            } else if ($scope.input < randomNumber) {
+                $scope.text = "The entered number is low";
+                $scope.input = "";
+
+            } else if ($scope.input > randomNumber) {
+                $scope.text = "The entered number is high";
+                $scope.input = "";
+            } else {
+                $scope.text = "Enter the valid Number";
+                $scope.input = "";
+            }
+
+        };
+
+
+        $scope.reloadGame = function() {
+            startGame();
+        }
+    };
+
+});
